a19e220934667fd4aca9c23fbf3f0b88c13f4741
leej1230 <leejaewoo1230@gmail.com>
2022-12-22 01:24:09 +0900

    done until nav bar

--- portfolio-proj/src/components/nav/Nav.jsx
+++ portfolio-proj/src/components/nav/Nav.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './nav.css'
+import {AiFillHome} from 'react-icons/ai'
+import {AiFillInfoCircle} from 'react-icons/ai'
+import {BiCodeBlock} from 'react-icons/bi'
+import {GrDocumentDownload} from 'react-icons/gr'
+import {GrContactInfo} from 'react-icons/gr'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav==='#' ? 'active' : ''}><AiFillHome /></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav==='#about' ? 'active' : ''}><AiFillInfoCircle /></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav==='#experience' ? 'active' : ''}><BiCodeBlock /></a>
+      <a href='#resume' onClick={() => setActiveNav('#resume')} className={activeNav==='#resume' ? 'active' : ''}><GrDocumentDownload /></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav==='#contact' ? 'active' : ''}><GrContactInfo /></a>
+    </nav>
   )
 }
 
